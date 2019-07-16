e6e997cff9859ba49bb556dcdf2e983bd624bbf4
Boris Cherkasky <36630728+borischerkasky@users.noreply.github.com>
2019-07-16 10:26:57 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,2 @@
 from flask_website import app
-print("Not working uah?")
-print("try this: https://stackoverflow.com/questions/26423984/unable-to-connect-to-flask-app-on-docker-from-host")
 app.run(debug=True)
